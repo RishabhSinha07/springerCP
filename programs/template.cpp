9739369902e1dde83ee4b475a3162b2a16e08074
Rishabh PC <rishabhsinha220@gmail.com>
2021-01-16 14:11:22 +0530

    first commit

---
+++ programs/template.cpp
@@ -0,0 +1,12 @@
+// g++ -std=c++11 -02 - Wall test.cpp -o test
+
+// This command produces a binary file test from the source code test.cpp. The
+// compiler follows the C++11 standard (-std=c++11), optimizes the code (-O2),
+// and shows warnings about possible errors (-Wall).
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	cout<<"It works";
+// solution comes here
+}
